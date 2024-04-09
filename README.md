4da1dc385d169f9d5b5432d8dd3f07f118a1170b
Jannis Maier <maierjan@hu-berlin.de>
2024-04-09 11:25:45 +0200

    Addign qo es for testing

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # HA-ES: Hardware-Aware Ensemble Selection
-This is WIP
+One of the concerns with currently existing ensemble selection algorithms is the size of the ensemble, which can affect the inference speed of the trained model. To address this issue, we introduce our novel approach: hardware-aware ensemble selection (HA-ES), which focuses on finding a balance in the performance and complexity trade-off inherent in ensembling.
+
+To evaluate our approach and compare it to the existing algorithms, we use TabRepo, which provides prediction probabilities for over 100 ML problems. We use this data to efficiently evaluate and compare the ensemble selection techniques.
+
+
+## Set-Up
+This set-up guide expects a Linux system.
+
+### (Optional) Create venv
+It's good practice to use a virtual environment. This isolates your project dependencies from global Python installations. This is how you create a virtual environment in your project directory:
+- `python3 -m venv venv`
+To activate it use:
+- `source venv/bin/activate`
+
+
+### Dependencies
+From the project root run the following commands to install the dependencies (`-e` to automatically install changes made to the code of the dependencies)
+- `python3 -m pip install -e extern/tabrepo`
+- `python3 -m pip install -e extern/phem`
+
+### Run test
+To run the current state of development use
+- `python3 haes/main.py`
--- haes/main.py
+++ haes/main.py
@@ -1,6 +1,7 @@
 from tabrepo import load_repository, EvaluationRepository
 from phem.methods.ensemble_selection import EnsembleSelection
 from phem.application_utils.supported_metrics import msc
+from phem.methods.ensemble_selection.qdo.qdo_es import QDOEnsembleSelection
 from sklearn.metrics import roc_auc_score
 
 import numpy as np
@@ -56,6 +57,46 @@ def expand_binary_predictions(predictions):
     expanded_predictions = np.stack([negative_class_probs, predictions], axis=-1)
     return expanded_predictions
 
+
+def evaluate_ensemble(name: str, ensemble: EnsembleSelection, repo: EvaluationRepository, task: str, predictions_val: list[np.ndarray], predictions_test: list[np.ndarray]):
+    dataset = repo.task_to_dataset(task)
+    fold = repo.task_to_fold(task)
+    
+    y_val = repo.labels_val(dataset=dataset, fold=fold)
+    # Format (n_configs, n_samples, n_classes)
+    predictions_val = np.array(predictions_val)
+    predictions_test = np.array(predictions_test)
+
+    number_of_classes = int(repo.dataset_metadata(dataset=dataset)["NumberOfClasses"]) # 0 for regression
+    if (number_of_classes == 2):
+        predictions_val = expand_binary_predictions(predictions_val)
+        predictions_test = expand_binary_predictions(predictions_test)
+
+    # Switch to simulating predictions on validation data  (i.e., the training data of the ensemble)
+    for bm in ensemble.base_models:
+        bm.switch_to_val_simulation()
+
+    print(f"Fitting {name} for task {task}, dataset {dataset}, fold {fold}")
+    ensemble.fit(predictions_val, y_val)
+
+    # Simulate to simulating predictions on test data  (i.e., the test data of the ensemble and base models)
+    for bm in ensemble.base_models:
+        bm.switch_to_test_simulation()
+
+    y_pred = ensemble.ensemble_predict_proba(predictions_test)
+    if number_of_classes == 2:
+        y_pred = y_pred[:, 1]
+    y_test = repo.labels_test(dataset=dataset, fold=fold)
+    print(f"\tROC AUC Test Score for {task}: {roc_auc_score(y_test, y_pred, multi_class='ovr', average='macro')}")
+    if(name == "GES"):
+        print(f"\tFinal ensemble size: {len(ensemble.indices_)}")
+        print(f"\tNumber of different base models in the ensemble: {len(set(ensemble.indices_))}")
+    elif(name == "QDO"):
+        print(f"\tFinal ensemble size: {len(ensemble.archive)}")
+        print(f"\tNumber of different base models in the ensemble: {len(set(ensemble.archive))}")
+    else: 
+        pass
+
 def main():
     # Define the context for the ensemble evaluation
     context_name = "D244_F3_C1530_30"
@@ -73,7 +114,6 @@ def main():
         for fold in folds
     ]
 
-
     # Setup the ensemble evaluation
     # for each task, evaluate the ensemble of size n
     ensemble_size = 100
@@ -89,7 +129,6 @@ def main():
             # Fetch predictions for each dataset and fold
             predictions_val.append(repo.predict_val(dataset=dataset, fold=fold, config=config))
             predictions_test.append(repo.predict_test(dataset=dataset, fold=fold, config=config))
-
 
             # Wrap predictions in the FakedFittedAndValidatedClassificationBaseModel
             # Note: You might need to adjust how predictions are combined or selected
@@ -120,34 +159,16 @@ def main():
             metric=msc(metric_name=metric_name, is_binary=is_binary, labels=labels),
             random_state=1,
         )
-
-        y_val = repo.labels_val(dataset=dataset, fold=fold)
-        # Format (n_configs, n_samples, n_classes)
-        predictions_val = np.array(predictions_val)
-        predictions_test = np.array(predictions_test)
-
-        if (number_of_classes == 2):
-            predictions_val = expand_binary_predictions(predictions_val)
-            predictions_test = expand_binary_predictions(predictions_test)
-
-        # Switch to simulating predictions on validation data  (i.e., the training data of the ensemble)
-        for bm in ges.base_models:
-            bm.switch_to_val_simulation()
-
-        print(f"Fitting ges for task {task}, dataset {dataset}, fold {fold}")
-        ges.fit(predictions_val, y_val)
-
-        # Simulate to simulating predictions on test data  (i.e., the test data of the ensemble and base models)
-        for bm in ges.base_models:
-            bm.switch_to_test_simulation()
-
-        y_pred = ges.ensemble_predict_proba(predictions_test)
-        if number_of_classes == 2:
-            y_pred = y_pred[:, 1]
-        y_test = repo.labels_test(dataset=dataset, fold=fold)
-        print(f"\tROC AUC Test Score for {task}: {roc_auc_score(y_test, y_pred, multi_class='ovr', average='macro')}")
-        print(f"\tFinal ensemble size: {len(ges.indices_)}")
-        print(f"\tNumber of different base models in the ensemble: {len(set(ges.indices_))}")
+        qdo = QDOEnsembleSelection(
+            base_models=base_models,
+            n_iterations=50,
+            score_metric=msc(metric_name=metric_name, is_binary=is_binary, labels=labels),
+            random_state=1,
+            archive_type="quality",
+        )
+        
+        evaluate_ensemble("GES", ges, repo, task, predictions_val, predictions_test)
+        evaluate_ensemble("QDO", qdo, repo, task, predictions_val, predictions_test)
 
 
 if __name__ == '__main__':
